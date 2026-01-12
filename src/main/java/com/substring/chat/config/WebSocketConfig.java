d81560cc4ce8d80e25fb94e866e862875d95bd02
Akash Sadhukhan <akashsadhukhan369@gmail.com>
2026-01-12 18:41:10 +0530

    Fix SockJS CORS for Render

--- src/main/java/com/substring/chat/config/WebSocketConfig.java
+++ src/main/java/com/substring/chat/config/WebSocketConfig.java
@@ -1,3 +1,38 @@
+//package com.substring.chat.config;
+//
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+//import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+//import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+//import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+//
+//
+//@Configuration
+//@EnableWebSocketMessageBroker
+//public class WebSocketConfig implements WebSocketMessageBrokerConfigurer
+//{
+//	@Override
+//	public void configureMessageBroker(MessageBrokerRegistry config) {
+//		
+//		config.enableSimpleBroker("/topic");
+//		// /topic/messages
+//		
+//		config.setApplicationDestinationPrefixes("/app");
+//		//app/chat
+//		
+//	}
+//	@Override
+//	public void registerStompEndpoints(StompEndpointRegistry registry) {
+//		
+//		registry.addEndpoint("/chat")
+//		.setAllowedOrigins("*")
+//		.withSockJS();
+//		
+//		// /chat endpoint par connection establish hoga
+//	}
+//}
+
+
 package com.substring.chat.config;
 
 import org.springframework.context.annotation.Configuration;
@@ -6,28 +41,20 @@ import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBr
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
-
 @Configuration
 @EnableWebSocketMessageBroker
-public class WebSocketConfig implements WebSocketMessageBrokerConfigurer
-{
-	@Override
-	public void configureMessageBroker(MessageBrokerRegistry config) {
-		
-		config.enableSimpleBroker("/topic");
-		// /topic/messages
-		
-		config.setApplicationDestinationPrefixes("/app");
-		//app/chat
-		
-	}
-	@Override
-	public void registerStompEndpoints(StompEndpointRegistry registry) {
-		
-		registry.addEndpoint("/chat")
-		.setAllowedOrigins("*")
-		.withSockJS();
-		
-		// /chat endpoint par connection establish hoga
-	}
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry config) {
+        config.enableSimpleBroker("/topic");
+        config.setApplicationDestinationPrefixes("/app");
+    }
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/chat")
+                .setAllowedOriginPatterns("*") // ⭐ THIS FIXES RENDER
+                .withSockJS();
+    }
 }
